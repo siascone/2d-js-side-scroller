8943c60104f660cbd3fa61dd61d51f2511a09190
siascone <spencer.iascone@gmail.com>
2023-04-27 15:07:25 -0600

    Complete basic objs addition

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # 2d-js-side-scroller 
  Built using Jessica Torres' Creating A Simple 2d Game With HTML5 & JavaScript
+
+ https://levelup.gitconnected.com/creating-a-simple-2d-game-with-html5-javascript-889aa06035ef
--- game.js
+++ game.js
@@ -3,6 +3,15 @@ const context = document.querySelector('canvas').getContext('2d')
 context.canvas.height = 400;
 context.canvas.width = 1200;
 
+// Start the fram count at 1 (also level 1)
+let frameCount = 1;
+
+// Set the number of obstacles to match the current "level" number
+let objCount = frameCount;
+
+// create a collection to hold the randomly generated x coordinates
+const objXCoords = [];
+
 const player = {
     height: 32,
     jumping: true,
@@ -66,6 +75,7 @@ const loop = function () {
         player.x = 1200;
     } else if (player.x > 1200) {
         player.x = -20;
+        nextFrame();
     }
 
     // creates teh backdrop for each frame
@@ -76,7 +86,27 @@ const loop = function () {
     context.beginPath();
     context.rect(player.x, player.y, player.width, player.height);
     context.fill();
-    // Creates the "Ground" for each fram
+    // Create the obstacles for each frame
+    // Set the standard obstacle height
+    const height = 200 * Math.cos(Math.PI / 6);
+    context.fillStyle = "pink";
+    objXCoords.forEach((objXCoord) => {
+        context.beginPath();
+
+        // (x = random, y = coord. on "ground")
+        context.moveTo(objXCoord, 385);
+
+        // (x = ^random +20, y = coord. on "ground")
+        context.lineTo(objXCoord + 20, 385);
+
+        // (x = ^random + 10, y = peak of triange)
+        context.lineTo(objXCoord + 10, 510 - height);
+
+        context.closePath();
+
+        context.fill();
+    });
+    // Creates the "Ground" for each frame
     context.strokeStyle = "#2e2532";
     context.lineWidth = 30;
     context.beginPath();
@@ -85,6 +115,18 @@ const loop = function () {
     context.stroke();
 
     window.requestAnimationFrame(loop);
+}
+
+// Create the obstacles for each fram
+
+const nextFrame = () => {
+    // increase the frame / "level" count
+    frameCount ++;
+
+    for (let i = 0; i < objCount; i++) {
+        let objXCoord = Math.floor(Math.random() * (1165 - 140 + 1) + 140);
+        objXCoords.push(objXCoord);
+    }
 }
 
 window.addEventListener("keydown", playerController.keyListener)
