aff86fb0172f6dbdb8dc4dce76c2b5b3bc9416bf
siascone <spencer.iascone@gmail.com>
2023-04-27 13:07:49 -0600

    Add player and controller objects

--- game.js
+++ game.js
@@ -3,3 +3,34 @@ const context = document.querySelector('canvas').getContext('2d')
 context.canvas.height = 400;
 context.canvas.width = 1220;
 
+const player = {
+    height: 32,
+    jumping: true,
+    width: 32,
+    x: 0,
+    xVelocity: 0,
+    y: 0,
+    yVelocity: 0
+};
+
+const playerController = {
+    left: false,
+    right: false,
+    up: false,
+
+    keyListener: function(e) {
+        let key_state = (e.type == "keydown") ? true : false;
+
+        switch (e.keyCode) {
+            case 37: // left arrow
+                controller.left = key_state;
+                break;
+            case 38: // up arrow
+                controller.up = key_state;
+                break;
+            case 39: // right arrow
+                controller.right = key_state;
+                break;
+        }
+    }
+}
